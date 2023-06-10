4990d715ed8b8ac892cd6d6a0c424738a990cc17
Gurtas <34601321+movlordjoaky@users.noreply.github.com>
2023-06-10 19:07:37 +0300

    Советы IDE по устранению warnings

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,13 +1,9 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
 
 public class Main {
 
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -13,9 +13,8 @@ public class UserDaoJDBCImpl implements UserDao, AutoCloseable {
 
     public void createUsersTable() {
         String query = "create table IF NOT EXISTS users(id int auto_increment, name VARCHAR(100) not null, last_name varchar(100) not null, age tinyint not null, constraint id primary key (id));";
+        try (Statement statement = Util.getConnection().createStatement()) {
 
-        try {
-            Statement statement = Util.getConnection().createStatement();
             statement.executeUpdate(query);
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -24,9 +23,7 @@ public class UserDaoJDBCImpl implements UserDao, AutoCloseable {
 
     public void dropUsersTable() {
         String query = "drop table IF EXISTS users;";
-
-        try {
-            Statement statement = Util.getConnection().createStatement();
+        try (Statement statement = Util.getConnection().createStatement()) {
             statement.executeUpdate(query);
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -36,9 +33,7 @@ public class UserDaoJDBCImpl implements UserDao, AutoCloseable {
 
     public void saveUser(String name, String lastName, byte age) {
         String query = "Insert Into users (name, last_name, age) values (?, ?, ?)";
-
-        try {
-            PreparedStatement statement = Util.getConnection().prepareStatement(query);
+        try (PreparedStatement statement = Util.getConnection().prepareStatement(query)) {
             statement.setString(1, name);
             statement.setString(2, lastName);
             statement.setInt(3, age);
@@ -50,8 +45,7 @@ public class UserDaoJDBCImpl implements UserDao, AutoCloseable {
 
     public void removeUserById(long id) {
         String query = "delete from users where id = ?";
-        try {
-            PreparedStatement statement = Util.getConnection().prepareStatement(query);
+        try (PreparedStatement statement = Util.getConnection().prepareStatement(query)) {
             statement.setLong(1, id);
             statement.executeUpdate();
         } catch (SQLException e) {
@@ -61,9 +55,8 @@ public class UserDaoJDBCImpl implements UserDao, AutoCloseable {
 
     public List<User> getAllUsers() {
         String query = "select id, name, last_name, age from users";
-        try {
+        try (Statement statement = Util.getConnection().createStatement()) {
             List<User> list = new ArrayList<>();
-            Statement statement = Util.getConnection().createStatement();
             ResultSet resultSet = statement.executeQuery(query);
             while (resultSet.next()) {
                 list.add(new User(
@@ -79,8 +72,8 @@ public class UserDaoJDBCImpl implements UserDao, AutoCloseable {
 
     public void cleanUsersTable() {
         String query = "delete from users";
-        try {
-            Statement statement = Util.getConnection().createStatement();
+        try (Statement statement = Util.getConnection().createStatement()) {
+
             statement.executeUpdate(query);
         } catch (SQLException e) {
             throw new RuntimeException(e);
