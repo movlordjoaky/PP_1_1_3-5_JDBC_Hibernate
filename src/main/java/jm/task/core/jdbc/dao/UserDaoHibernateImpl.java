830d43b00f016248d8b99be8d275b0251037c3fe
Gurtas <34601321+movlordjoaky@users.noreply.github.com>
2023-07-17 13:53:16 +0300

    sessionFactory в конструкторе

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -10,14 +10,15 @@ import org.hibernate.query.Query;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
-    public UserDaoHibernateImpl() {
+    private SessionFactory sessionFactory;
 
+    public UserDaoHibernateImpl() {
+        this.sessionFactory = Util.getSessionFactory();
     }
 
     @Override
     public void createUsersTable() {
-        try (SessionFactory sessionFactory = Util.getSessionFactory();
-             Session session = sessionFactory.openSession()) {
+        try (Session session = this.sessionFactory.openSession()) {
             Transaction transaction = session.beginTransaction();
             String query = "CREATE TABLE IF NOT EXISTS users(id INT AUTO_INCREMENT, name VARCHAR(100) NOT NULL , lastName VARCHAR(100) NOT NULL , age TINYINT NOT NULL , CONSTRAINT id PRIMARY KEY (id));";
             session.createSQLQuery(query).executeUpdate();
@@ -29,12 +30,12 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void dropUsersTable() {
-        try (SessionFactory sessionFactory = Util.getSessionFactory();
-             Session session = sessionFactory.openSession()) {
+        try (Session session = this.sessionFactory.openSession()) {
             Transaction transaction = session.beginTransaction();
             String query = "DROP TABLE IF EXISTS users;";
             session.createSQLQuery(query).executeUpdate();
             transaction.commit();
+            session.close();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -42,8 +43,7 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        try (SessionFactory sessionFactory = Util.getSessionFactory();
-             Session session = sessionFactory.openSession()) {
+        try (Session session = this.sessionFactory.openSession()) {
             Transaction transaction = session.beginTransaction();
             String query = "INSERT INTO users (name, lastName, age) VALUES (:name, :lastName, :age)";
             session.createSQLQuery(query)
@@ -59,8 +59,7 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void removeUserById(long id) {
-        try (SessionFactory sessionFactory = Util.getSessionFactory();
-             Session session = sessionFactory.openSession()) {
+        try (Session session = this.sessionFactory.openSession()) {
             Transaction transaction = session.beginTransaction();
             String query = "DELETE FROM users WHERE id = :id";
             session.createSQLQuery(query)
@@ -74,8 +73,7 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public List<User> getAllUsers() {
-        try (SessionFactory sessionFactory = Util.getSessionFactory();
-             Session session = sessionFactory.openSession()) {
+        try (Session session = this.sessionFactory.openSession()) {
             Query query = session.createQuery("FROM User", User.class);
             List<User> list = query.list();
             session.close();
@@ -87,13 +85,13 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void cleanUsersTable() {
-        try (SessionFactory sessionFactory = Util.getSessionFactory();
-             Session session = sessionFactory.openSession()) {
+        try (Session session = this.sessionFactory.openSession()) {
             Transaction transaction = session.beginTransaction();
             String query = "DELETE FROM users";
             session.createSQLQuery(query)
                     .executeUpdate();
             transaction.commit();
+            session.close();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
